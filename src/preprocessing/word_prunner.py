b039fa499e711be69fafa5a6606359296df35d51
weirdwizardthomas <thomas.koristka@gmail.com>
2020-03-06 23:47:51 +0100

    Added a word prunner to filter out stop words and non alphabetical words (i.e. punctuation)

---
+++ src/preprocessing/word_prunner.py
@@ -0,0 +1,12 @@
+import string
+
+from nltk.corpus import stopwords
+
+
+class WordPrunner:
+    def __init__(self):
+        self.stop_words = set(stopwords.words('english'))
+
+    def prune(self, tokens: list) -> list:
+        # remove stop words and punctuation
+        return [term for term in tokens if term.isalpha() and term not in self.stop_words]
