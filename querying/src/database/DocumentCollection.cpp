0a2eb0cf49c21c3d89d8210d8fc8ee5d15ea5a9b
Lukáš Paukert <paukeluk@fit.cvut.cz>
2020-04-01 23:16:14 +0200

    Fixed getting file name query

--- querying/src/database/DocumentCollection.cpp
+++ querying/src/database/DocumentCollection.cpp
@@ -7,19 +7,19 @@ DocumentCollection::DocumentCollection(const string &path)
         : db(path) {}
 
 std::vector<Document> DocumentCollection::fetchCollection() {
-    SQLite::Statement query(db, "SELECT id,filename FROM Document");
+    SQLite::Statement query(db, "SELECT id, filename FROM Document");
 
     vector<Document> result;
 
     while (query.executeStep())
-        result.emplace_back(query.getColumn(0), query.getColumn(1));
+        result.emplace_back(query.getColumn("id"), query.getColumn("filename"));
 
     return result;
 }
 
 Document DocumentCollection::getDocumentByID(int id) {
-    SQLite::Statement query(db, "SELECT filename FROM Document WHERE :id = 389");
-    query.bind(":id", id);
+    SQLite::Statement query(db, "SELECT filename FROM Document WHERE id = :document_id");
+    query.bind(":document_id", id);
     query.executeStep();
 
     return {id, query.getColumn("filename")};
