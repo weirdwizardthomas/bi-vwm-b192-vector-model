6c371daaa2956602ca26723ef361ac1097277bd8
Lukáš Paukert <paukeluk@fit.cvut.cz>
2020-04-12 14:54:06 +0200

    Renamed MainPage to Page (one file was not staged)

--- querying/src/main.cpp
+++ querying/src/main.cpp
@@ -8,7 +8,7 @@
 #include "calculation/Computor.h"
 #include "util/QueryJSONParser.h"
 #include "util/InvertedIndexJSONParser.h"
-#include "src/ui/MainPage.h"
+#include "src/ui/Page.h"
 #include "src/database/Database.h"
 #include "src/database/Document.h"
 
@@ -17,7 +17,7 @@ using namespace cxxopts;
 
 std::unique_ptr<Wt::WApplication> createApplication(const Wt::WEnvironment& env)
 {
-  auto app = Wt::cpp14::make_unique<MainPage>(env);
+  auto app = Wt::cpp14::make_unique<Page>(env);
   app->setTitle("Querying: Vector model of information retrieval");
   app->useStyleSheet("https://fonts.googleapis.com/css?family=Open+Sans&display=swap");
   app->useStyleSheet("style.css");
@@ -50,27 +50,3 @@ int main(int argc, char *argv[]) {
     auto availableDocuments = collection.fetchCollection();
     */
 }
-
-/*
- todo tohle bylo puvodne smycka, ve ktery se dostavalo: 1. flag, jestli se ma skoncit, 2. cesta k jsonu, ktery reprezentuje dotaz
- todo 3. threshold
- todo misto smycky je to potreba dat jako nejaky event, napr otevreni dokumentu, nebo jeho pretazeni do prohlizece dokumentu, nebo nejake tlacitko
- todo taky to nemusi byt json ten vstup, ale to je na povazenou, jak to udelat. Protoze pokud bude vstup dokument, tak by asi mel projit taky preprocessingem a vypoctem vah
-
-    string queryPath;
-    bool exit = false;
-    double threshold;
-
-
-    while (true) {
-        cin >> exit;
-        if (exit)
-            break;
-
-        cin >> queryPath >> threshold;
-        Query query(QueryJSONParser(queryPath).parse(), threshold);
-        auto res = Computor(space, query).compute();
-     }
-
-
-*/
